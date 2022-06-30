6e20a172d8b17beb60b1d59fb0c3281bfcb4de32
PhuongThao2002 <90228580+PhuongThao2002@users.noreply.github.com>
2022-07-01 02:13:43 +0700

    Add files via upload

--- SQL_DKTC.sql
+++ SQL_DKTC.sql
@@ -33,6 +33,7 @@ SiSo int,
 GiangVien nvarchar(30),
 PhongHoc varchar(10),
 TietHoc varchar(30),
+SSHienTai int
 )
 drop table tblLopHP;
 
@@ -41,6 +42,12 @@ MaSV char(10) foreign key references tblSV(MaSV),
 MaLopHP char(10) foreign key references tblMonHoc(MaMon)
 )
 drop table tblKQDangKi;
+
+create table tblQL(
+MaQL char(10) primary key,
+TenQL nvarchar(50),
+MatKhauQL char(30),
+)
 
 insert into tblNhomMonHoc values ('HTQL',N'Hệ thống thông tin quản lý');
 insert into tblNhomMonHoc values ('KT',N'Kế toán');
@@ -66,27 +73,32 @@ insert into tblMonHoc values('KTTC',N'Kế toán tài chính',3,'KT');
 select * from tblMonHoc;
 delete from tblMonHoc;
 
-insert into tblLopHP values('PTUD-01','PTUD',N'Phát triển các ứng dụng trong quản lý 01',60,'Trinh Hoai Son','A2-601','Thu 2, tiet 3-4');
-insert into tblLopHP values('PTUD-02','PTUD',N'Phát triển các ứng dụng trong quản lý 02',60,'Trinh Hoai Son','A2-601','Thu 4, tiet 5-6');
-insert into tblLopHP values('LTNC-01','LTNC',N'Lập trình nâng cao 01',40,'Nguyen Van Thu','A2-603','Thu 5, tiet 7-8');
-insert into tblLopHP values('UDCSDL-01','UDCSDL',N'Ứng dụng CSDL 01',60,'Pham Van Minh','A2-610','Thu 4, tiet 5-6');
-insert into tblLopHP values('UDCSDL-02','UDCSDL',N'Ứng dụng CSDL 02',60,'Pham Van Minh','A2-610','Thu 3, tiet 1-2');
-insert into tblLopHP values('NLKT-01','NLKT',N'Nguyên lý kế toán 01',60,'Nguyen Thi Tuyet','D-105','Thu 6, tiet 7-8');
-insert into tblLopHP values('NLKT-02','NLKT',N'Nguyên lý kế toán 02',60,'Tran Thi Huong','D-103','Thu 5, tiet 3-4');
-insert into tblLopHP values('NLKT-03','NLKT',N'Nguyên lý kế toán 03',60,'Phung Van Huy','D-205','Thu 4, tiet 1-2');
-insert into tblLopHP values('THMLN-01','THMLN',N'Triết học Mác - Lênin 01',60,'Nguyen Thi Hieu','C-101','Thu 4, tiet 1-2');
-insert into tblLopHP values('THMLN-02','THMLN',N'Triết học Mác - Lênin 02',60,'Bui Thi Phuong','D-305','Thu 5, tiet 3-4');
-insert into tblLopHP values('KTCT-01','KTCT',N'Kinh tế chính trị 01',60,'Tran Hoang Son','C-204','Thu 5, tiet 3-4');
-insert into tblLopHP values('KTCT-02','KTCT',N'Kinh tế chính trị 02',60,'Cao Xuan Tu','C-201','Thu 6, tiet 1-2');
-insert into tblLopHP values('KTQT-01','KTQT',N'Kế toán quản trị 01',60,'Tran Ngoc Quynh','D1-401','Thu 4, tiet 5-6');
-insert into tblLopHP values('KTDN-01','KTDN',N'Kế toán doanh nghiệp',60,'Le Huyen Tram','D2-306','Thu 3, tiet 5-6');
-insert into tblLopHP values('KTTC-01','KTTC',N'Kế toán tài chính',60,N'Nguyễn Quang Chiến','D2-306','Thu 5, tiet 1-2');
+insert into tblLopHP values('PTUD-01','PTUD',N'Phát triển các ứng dụng trong quản lý 01',60,'Trinh Hoai Son','A2-601','Thu 2, tiet 3-4',0);
+insert into tblLopHP values('PTUD-02','PTUD',N'Phát triển các ứng dụng trong quản lý 02',60,'Trinh Hoai Son','A2-601','Thu 4, tiet 5-6',0);
+insert into tblLopHP values('LTNC-01','LTNC',N'Lập trình nâng cao 01',40,'Nguyen Van Thu','A2-603','Thu 5, tiet 7-8',0);
+insert into tblLopHP values('UDCSDL-01','UDCSDL',N'Ứng dụng CSDL 01',60,'Pham Van Minh','A2-610','Thu 4, tiet 5-6',0);
+insert into tblLopHP values('UDCSDL-02','UDCSDL',N'Ứng dụng CSDL 02',60,'Pham Van Minh','A2-610','Thu 3, tiet 1-2',0);
+insert into tblLopHP values('NLKT-01','NLKT',N'Nguyên lý kế toán 01',60,'Nguyen Thi Tuyet','D-105','Thu 6, tiet 7-8',0);
+insert into tblLopHP values('NLKT-02','NLKT',N'Nguyên lý kế toán 02',60,'Tran Thi Huong','D-103','Thu 5, tiet 3-4',0);
+insert into tblLopHP values('NLKT-03','NLKT',N'Nguyên lý kế toán 03',60,'Phung Van Huy','D-205','Thu 4, tiet 1-2',0);
+insert into tblLopHP values('THMLN-01','THMLN',N'Triết học Mác - Lênin 01',60,'Nguyen Thi Hieu','C-101','Thu 4, tiet 1-2',0);
+insert into tblLopHP values('THMLN-02','THMLN',N'Triết học Mác - Lênin 02',60,'Bui Thi Phuong','D-305','Thu 5, tiet 3-4',0);
+insert into tblLopHP values('KTCT-01','KTCT',N'Kinh tế chính trị 01',60,'Tran Hoang Son','C-204','Thu 5, tiet 3-4',0);
+insert into tblLopHP values('KTCT-02','KTCT',N'Kinh tế chính trị 02',60,'Cao Xuan Tu','C-201','Thu 6, tiet 1-2',0);
+insert into tblLopHP values('KTQT-01','KTQT',N'Kế toán quản trị 01',60,'Tran Ngoc Quynh','D1-401','Thu 4, tiet 5-6',0);
+insert into tblLopHP values('KTDN-01','KTDN',N'Kế toán doanh nghiệp',60,'Le Huyen Tram','D2-306','Thu 3, tiet 5-6',0);
+insert into tblLopHP values('KTTC-01','KTTC',N'Kế toán tài chính',60,N'Nguyễn Quang Chiến','D2-306','Thu 5, tiet 1-2',0);
 select * from tblLopHP;
 delete from tblLopHP;
 
+insert into tblQL values ('111222','Nguyen Van Truong','456789');
+insert into tblQL values ('444555','Tran Dinh Hoang','456789');
+select * from tblQL;
 
+Select MaMon, TenMon, SoTC from tblMonHoc,tblSV where tblSV.MaNhomMonHoc=tblMonHoc.MaNhomMonHoc and tblSV.MaSV = '11201234';
 
+select * from tblLopHP,tblMonHoc where tblLopHP.MaMon = tblMonHoc.MaMon and tblLopHP.MaMon='KTCT';
 
-
+select * from tblLopHP where MaMon='KTCT';
 
 
